906185eb86b30a6a2196bcc190d111153a0f61ac
Kyle Smith <sKorpion19091@gmail.com>
2022-05-04 22:32:31 +0200

    ✅ (client) Added hero test

---
+++ client/components/Hero.jsx
@@ -0,0 +1,17 @@
+const Hero = () => {
+    return (
+        <div className="hero min-h-[80vh]">
+            <div className="hero-content">
+                <div className="max-w-md">
+                    <h1 className="text-5xl font-bold">Hello there 👋</h1>
+                    <p className="py-6">
+                        V Ʌ S is an intelligent visitor management system aiming to bring an efficient and customizable interface for our clients.
+                    </p>
+                    <button className="btn btn-primary">Get Started</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
