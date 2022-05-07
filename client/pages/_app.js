8c68989d4b229d3f1d8b74f3b1be5d9dba039aab
Kyle Smith <sKorpion19091@gmail.com>
2022-05-07 13:18:20 +0200

    🧶 (client) Added lint rules

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,31 +1,30 @@
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 
-import useAuth from "../store/authStore"
+import useAuth from "../store/authStore";
 
 import Layout from "../components/Layout";
 
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-
     const router = useRouter();
     const access_token = useAuth((state) => state.access_token);
 
     const isPublicPath = (url) => {
-        const publicPaths = ['/login', '/'];
-        const path = url.split('?')[0];
+        const publicPaths = ["/login", "/"];
+        const path = url.split("?")[0];
         return publicPaths.includes(path);
     };
-    
+
     useEffect(() => {
-        if(!isPublicPath(router.asPath) && !access_token) {
+        if (!isPublicPath(router.asPath) && !access_token) {
             router.push("/login");
         }
-    }, [])
+    }, [router, access_token]);
 
-    if(pageProps.protected && !access_token) {
-        return <Layout>Unauthorized</Layout>
+    if (pageProps.protected && !access_token) {
+        return <Layout>Unauthorized</Layout>;
     }
 
     return <Component {...pageProps} />;
--- client/pages/login.jsx
+++ client/pages/login.jsx
@@ -1,117 +1,153 @@
 import { useRouter } from "next/router";
 import { useState } from "react";
 import { Formik } from "formik";
-import {
-	ApolloClient,
-	InMemoryCache,
-	gql
-} from "@apollo/client";
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 
 import Layout from "../components/Layout";
 
 import useAuth from "../store/authStore";
 
 const Login = () => {
+    const login = useAuth((state) => state.login);
+    const logout = useAuth((state) => state.logout);
+    const router = useRouter();
 
-	const login = useAuth((state) => state.login);
-	const logout = useAuth((state) => state.logout);
-	const router = useRouter();
+    const [showErrorAlert, setShowErrorAlert] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
 
-	const [showErrorAlert, setShowErrorAlert] = useState(false);
-	const [errorMessage, setErrorMessage] = useState("");
+    return (
+        <Layout>
+            <div className="relative flex h-full min-h-[80vh] w-full flex-col items-center justify-center overflow-hidden shadow">
+                <Formik
+                    initialValues={{ email: "", password: "" }}
+                    validate={(values) => {
+                        const errors = {};
+                        if (!values.email) {
+                            errors.email = "Required";
+                        } else if (
+                            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(
+                                values.email
+                            )
+                        ) {
+                            errors.email = "Invalid email address";
+                        }
+                        return errors;
+                    }}
+                    onSubmit={(values, { setSubmitting }) => {
+                        const client = new ApolloClient({
+                            uri: process.env.BACKEND_GRAPHQL_URL,
+                            cache: new InMemoryCache(),
+                        });
 
-	return (
-		<Layout>
-			<div className="w-full min-h-[80vh] h-full flex flex-col justify-center items-center shadow relative overflow-hidden">
-				<Formik
-					initialValues={{ email: '', password: '' }}
-					validate={values => {
-						const errors = {};
-						if (!values.email) {
-							errors.email = 'Required';
-						} else if (
-							!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-						) {
-							errors.email = 'Invalid email address';
-						}
-						return errors;
-					}}
-					onSubmit={(values, { setSubmitting }) => {
-						const client = new ApolloClient({
-							uri: process.env.BACKEND_GRAPHQL_URL,
-							cache: new InMemoryCache()
-						});
-
-						client.mutate(
-							{
-								mutation: gql`
+                        client
+                            .mutate({
+                                mutation: gql`
 									mutation {
 										login(email: "${values.email}", password: "${values.password}") {
 											access_token
 										}
 									 }
-								`
-							}
-						).then(result => {
-							// Enable button
-							setSubmitting(false);
+								`,
+                            })
+                            .then((result) => {
+                                // Enable button
+                                setSubmitting(false);
 
-							// Remove alerts if any
-							setShowErrorAlert(false);
+                                // Remove alerts if any
+                                setShowErrorAlert(false);
 
-							// Get token from response
-							const token = result.data?.login.access_token;
+                                // Get token from response
+                                const token = result.data?.login.access_token;
 
-							logout();
+                                logout();
 
-							// Add token to store
-							login(token);
+                                // Add token to store
+                                login(token);
 
-							router.push("/createInvite");
+                                router.push("/createInvite");
+                            })
+                            .catch((err) => {
+                                setShowErrorAlert(true);
+                                setSubmitting(false);
+                                setErrorMessage(err.message);
+                            });
+                    }}
+                >
+                    {({
+                        values,
+                        errors,
+                        touched,
+                        handleChange,
+                        handleBlur,
+                        handleSubmit,
+                        isSubmitting,
+                    }) => (
+                        <form
+                            onSubmit={handleSubmit}
+                            className="prose form-control space-y-4 rounded-xl bg-base-300 p-14 md:p-28"
+                        >
+                            <h1>Welcome Back 👋</h1>
+                            <input
+                                type="email"
+                                name="email"
+                                placeholder="Email"
+                                autoComplete="username"
+                                className="input input-bordered w-full max-w-xs"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.email}
+                            ></input>
+                            <span className="text-error">
+                                {errors.email && touched.email && errors.email}
+                            </span>
+                            <input
+                                type="password"
+                                name="password"
+                                autoComplete="current-password"
+                                placeholder="Password"
+                                className="input input-bordered w-full max-w-xs"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.password}
+                            ></input>
+                            <button
+                                className="btn btn-primary"
+                                type="submit"
+                                disabled={isSubmitting}
+                            >
+                                Submit
+                            </button>
+                        </form>
+                    )}
+                </Formik>
 
-						}).catch(err => {
-							setShowErrorAlert(true);
-							setSubmitting(false);
-							setErrorMessage(err.message);
-						});
-					}}
-				>
-					{({
-						values,
-						errors,
-						touched,
-						handleChange,
-						handleBlur,
-						handleSubmit,
-						isSubmitting,
-					}) => (
-						<form onSubmit={handleSubmit} className="form-control bg-base-300 space-y-4 p-14 md:p-28 rounded-xl prose">
-							<h1>Welcome Back 👋</h1>
-							<input type="email" name="email" placeholder="Email" autoComplete="username" className="input input-bordered w-full max-w-xs" onChange={handleChange}
-								onBlur={handleBlur}
-								value={values.email}></input>
-							<span className="text-error">{errors.email && touched.email && errors.email}</span>
-							<input type="password" name="password" autoComplete="current-password" placeholder="Password" className="input input-bordered w-full max-w-xs" onChange={handleChange}
-								onBlur={handleBlur}
-								value={values.password}></input>
-							<button className="btn btn-primary" type="submit" disabled={isSubmitting}>
-								Submit
-							</button>
-						</form>
-					)}
-				</Formik>
-
-				<div className={`absolute top-3 drop-alert ${showErrorAlert ? "active" : ""}`}>
-					<div className="alert alert-error shadow-lg">
-						<div>
-							<svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-							<span>Error! {errorMessage}</span>
-						</div>
-					</div>
-				</div>
-			</div>
-		</Layout >
-	);
-}
+                <div
+                    className={`drop-alert absolute top-3 ${
+                        showErrorAlert ? "active" : ""
+                    }`}
+                >
+                    <div className="alert alert-error shadow-lg">
+                        <div>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-6 w-6 flex-shrink-0 stroke-current"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                                />
+                            </svg>
+                            <span>Error! {errorMessage}</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Layout>
+    );
+};
 
 export default Login;
