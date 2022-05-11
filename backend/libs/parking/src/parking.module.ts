da3457b5ca0b9496dc50ed5d91f3706698e23acc
LarisaBotha <84624573+LarisaBotha@users.noreply.github.com>
2022-05-11 11:07:36 +0200

    imports

--- backend/libs/parking/src/parking.module.ts
+++ backend/libs/parking/src/parking.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ParkingService } from './parking.service';
+import { MongooseModule } from "@nestjs/mongoose";
+import { Parking, ParkingSchema } from "./schema/parking.schema";
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Parking.name, schema: ParkingSchema },
+    ]),
+  ],
   providers: [ParkingService],
   exports: [ParkingService],
 })
