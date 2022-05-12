3fe246ccc54e9b5447f6b4a3a0a69d9e186122be
Tabby709 <u19294418@tuks.co.za>
2022-05-12 10:06:56 +0200

    ✅ (tests):added unit tests for service layer

--- backend/libs/visitor/src/visitor.service.spec.ts
+++ backend/libs/visitor/src/visitor.service.spec.ts
@@ -2,9 +2,18 @@ import { getModelToken } from "@nestjs/mongoose";
 import { Test, TestingModule } from "@nestjs/testing";
 import { Visitor } from "./schema/visitor.schema";
 import { VisitorService } from "./visitor.service";
+import {Model} from "mongoose";
 
 describe("VisitorService", () => {
     let service: VisitorService;
+    let model: Model<Visitor>;
+    let visitorModelMock={
+         find:jest.fn(()=>({
+        exec: jest.fn(()=>({data:[]}))
+    })),
+    create:jest.fn(()=>({data:{}})),
+
+    }
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
