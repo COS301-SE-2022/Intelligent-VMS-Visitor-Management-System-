40b691ed16cd00a31ae21b1177202b2da3061abc
LarisaBothaTuks <u20522623@tuks.co.za>
2022-05-12 11:16:49 +0200

    ✨ 🎉 (parking): Added schema and commands

---
+++ backend/libs/parking/src/commands/handlers/assignParkingCommand.handler.ts
@@ -0,0 +1,29 @@
+import { AssignParkingCommand } from "../impl/assignParking.command";
+import { CommandHandler, ICommandHandler } from "@nestjs/cqrs";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { Parking, ParkingDocument } from "../../schema/parking.schema";
+
+
+@CommandHandler(AssignParkingCommand)
+export class FreeParkingCommandHandler implements ICommandHandler<AssignParkingCommand> {
+  constructor(
+      @InjectModel(Parking.name) private parkingModel: Model<ParkingDocument>,
+  ) {}
+
+  async execute(command: AssignParkingCommand) {
+
+    //db stuff for assigning parking
+    const { visitorEmail, parkingNumber } = command;
+
+    //TODO (Larisa) change find one
+    return await this.parkingModel.findOneAndUpdate({parkingNumber: parkingNumber}, {visitorEmail: visitorEmail},function (err, docs) {
+      if (err){
+          console.log(err)
+      }
+      else{
+          console.log("Parking assigned : ", docs);
+      }
+    });
+  }
+}
