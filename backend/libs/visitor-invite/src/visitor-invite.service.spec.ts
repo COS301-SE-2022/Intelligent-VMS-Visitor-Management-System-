ddfa02c8ba6395915255045d27edd9c23a1bcc76
Kyle Smith <sKorpion19091@gmail.com>
2022-05-16 09:47:03 +0200

    ✅ (backend) Updated invite unit tests

--- backend/libs/visitor-invite/src/visitor-invite.service.spec.ts
+++ backend/libs/visitor-invite/src/visitor-invite.service.spec.ts
@@ -1,25 +1,98 @@
 import { Test, TestingModule } from "@nestjs/testing";
-import { CommandBus, QueryBus } from "@nestjs/cqrs";
+import { CommandBus, IQuery, QueryBus } from "@nestjs/cqrs";
 import { VisitorInviteService } from "./visitor-invite.service";
+import {GetInvitesQuery} from "./queries/impl/getInvites.query";
+import {GetNumberVisitorQuery} from "./queries/impl/getNumberOfVisitors.query";
 
 describe("VisitorInviteService", () => {
     let service: VisitorInviteService;
-    let command: CommandBus;
+
+    const queryBusMock = {
+        execute: jest.fn((query: IQuery) => {
+            if(query instanceof GetInvitesQuery) {
+                if(query.email === "admin@mail.com") {
+                    return [
+                        {
+                            visitorEmail: "visitor@mail.com",
+                            residentEmail: "admin@mail.com",
+                            idDocType: "RSA-ID",
+                            idNumber: "0109195283090",
+                            inviteID: "fej1-23d3-334f-99fd",
+                            requiresParking: true
+                        },
+                        {
+                            visitorEmail: "visitor2@mail.com",
+                            residentEmail: "admin@mail.com",
+                            idDocType: "RSA-ID",
+                            idNumber: "0109195283090",
+                            inviteID: "fej1-33ft-334f-99fd",
+                            requiresParking: false
+                        }
+                    ]
+                } else {
+                    return [];
+                }
+            } else if(query instanceof GetNumberVisitorQuery) {
+                return 898;
+            }
+        }),
+    };
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            providers: [VisitorInviteService, CommandBus, QueryBus],
+            providers: [
+                VisitorInviteService, 
+                CommandBus, 
+                {
+                    provide: QueryBus, useValue: queryBusMock
+                },
+            ],
         }).compile();
 
         await module.init()
 
         service = module.get<VisitorInviteService>(VisitorInviteService);
-        command = module.get<CommandBus>(CommandBus);
     });
 
     it("should be defined", () => {
         expect(service).toBeDefined();
-        expect(command).toBeDefined();
+    });
+
+    describe("getInvites", () => {
+        it("should return invites of the current user", async () => {
+            const user = {
+                email: "admin@mail.com",
+                permission: 0
+            }
+
+            const invites = await service.getInvites(user.email);
+
+            expect(invites).toHaveLength(2);
+            expect(invites[0]).toHaveProperty("visitorEmail");
+            expect(invites[0]).toHaveProperty("residentEmail");
+            expect(invites[0]).toHaveProperty("idDocType");
+            expect(invites[0]).toHaveProperty("inviteID");
+            expect(invites[0]).toHaveProperty("requiresParking");
+        });
+
+        it("should return an empty array when accessing invites for another user", async () => {
+            const user = {
+                email: "otheruser@mail.com",
+                permission: 1
+            }
+
+            const invites = await service.getInvites(user.email);
+
+            expect(invites).toHaveLength(0);
+        });
+
+    });
+
+    describe("getTotalNumberOfVisitors", () => {
+        it("should return total number open invites sent for building", async () => {
+            const numInvitesSent = await service.getTotalNumberOfVisitors();
+            expect(numInvitesSent).toEqual(898);
+        });
     });
 
 });
