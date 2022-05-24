bc3a7b7ac7b6ad52c7809071eb6ef81b3b612f92
Kyle Smith <sKorpion19091@gmail.com>
2022-05-24 20:10:44 +0200

    ⚰️ (client) Moved integration folder

--- client/__tests__/integration/createInvite.test.js
+++ client/__tests__/integration/createInvite.test.js
@@ -4,8 +4,8 @@ import userEvent from '@testing-library/user-event';
 import "@testing-library/jest-dom";
 import { MockedProvider } from "@apollo/client/testing";
 
-import Login from "../pages/Login";
-import useAuth from "../store/authStore";
+import Login from "../../pages/Login";
+import useAuth from "../../store/authStore";
 
 describe("create invite", () => {
     it("user can create an invite after logging in", async () => {
