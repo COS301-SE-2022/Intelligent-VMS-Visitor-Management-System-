9ec80bca5604e742ef06153eb354cebc45363e1b
Kyle Smith <sKorpion19091@gmail.com>
2022-05-27 17:58:49 +0200

    ✨ (client) Added get invites in date range query backend connection

--- client/pages/adminDashboard.jsx
+++ client/pages/adminDashboard.jsx
@@ -9,6 +9,12 @@ import { BsFillHouseDoorFill } from "react-icons/bs";
 
 import useAuth from "../store/authStore";
 
+const getFormattedDateString = (date) => {
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    return [date.getFullYear(), (month > 9 ? "" : "0") + month, (day > 9 ? "" : "0") + day].join("-");
+};
+
 const AdminDashboard = () => {
     const [numInvitesSent, setNumInvitesSent] = useState(0);
     const numParkingSpotsAvailable = useAuth((state) => state.numParkingSpots);
@@ -17,24 +23,51 @@ const AdminDashboard = () => {
     const updateParkingSpots = useAuth((state) => state.updateParkingSpots);
     const decodedToken = useAuth((state) => state.decodedToken)();
     const router = useRouter();
+    const startDate = new Date(Date.now());
+    const endDate = new Date(startDate);
+    endDate.setDate(endDate.getDate() + 9);
 
-    const { loading, error, data } = useQuery(gql`
+    const numInvitesQuery = useQuery(gql`
         query {
             getTotalNumberOfVisitors
         }
     `);
 
+    const numInviteInDateRangeQuery = useQuery(gql`
+        query {
+            getNumInvitesPerDate(
+                dateStart: "${getFormattedDateString(startDate)}",
+                dateEnd: "${getFormattedDateString(endDate)}"
+            ) {
+                inviteDate
+            }
+        }
+    `);
+
     useEffect(() => {
-        if (!loading && !error) {
-            const invites = data.getTotalNumberOfVisitors;
+        if (!numInvitesQuery.loading && !numInvitesQuery.error) {
+            const invites = numInvitesQuery.data.getTotalNumberOfVisitors;
             setNumInvitesSent(invites);
-        } else if (error) {
+        } else if (numInvitesQuery.error) {
             if (error.message === "Unauthorized") {
                 router.push("/expire");
                 return;
             }
         }
-    }, [loading, error, data, router]);
+
+        if(!numInviteInDateRangeQuery.loading && !numInviteInDateRangeQuery.error) {
+            console.log(getFormattedDateString(startDate), getFormattedDateString(endDate));
+            const invites = numInviteInDateRangeQuery.data.getNumInvitesPerDate;
+            console.log(invites);
+        } else if(numInviteInDateRangeQuery.error) {
+            if (numInviteInDateRangeQuery.error.message === "Unauthorized") {
+                router.push("/expire");
+                return;
+            }
+            console.log(numInviteInDateRangeQuery.error);
+        }
+
+    }, [numInvitesQuery, numInviteInDateRangeQuery, router]);
 
     return (
         <Layout>
@@ -51,10 +84,9 @@ const AdminDashboard = () => {
                             <div className="place-center grid grid-cols-1 md:grid-cols-3 grid-rows-1 gap-4">
                                 <div className="card flex flex-col items-center justify-center space-y-5">
                                     <div className="avatar">
-                                        <button className="btn btn-disabled btn-circle bg-base-100 p-2">
+                                        <button className="btn btn-disabled btn-circle bg-base-100 text-3xl flex items-center">
                                             <BsFillHouseDoorFill
                                                 className="text-primary"
-                                                size="lg"
                                             />
                                         </button>
                                     </div>
@@ -69,10 +101,9 @@ const AdminDashboard = () => {
 
                                 <div className="card flex flex-col items-center justify-center space-y-5">
                                     <div className="avatar">
-                                        <button className="btn btn-disabled btn-circle bg-base-100 p-2">
+                                        <button className="btn btn-disabled btn-circle bg-base-100 text-3xl">
                                             <AiFillEye
                                                 className="text-primary"
-                                                size="lg"
                                             />
                                         </button>
                                     </div>
@@ -85,10 +116,9 @@ const AdminDashboard = () => {
 
                                 <div className="card flex flex-col items-center justify-center space-y-5">
                                     <div className="avatar">
-                                        <button className="btn btn-disabled btn-circle bg-base-100 p-2">
+                                        <button className="btn btn-disabled btn-circle bg-base-100 p-2 text-3xl">
                                             <AiFillEye
                                                 className="text-primary"
-                                                size="lg"
                                             />
                                         </button>
                                     </div>
@@ -105,12 +135,11 @@ const AdminDashboard = () => {
                         <h1 className="text-sm md:text-xl lg:text-2xl">Number of Parking Spots Available</h1>
                         <div className="flex select-none space-x-4">
                             <button
-                                className="btn btn-circle p-2"
+                                className="btn btn-circle text-3xl"
                                 onClick={incParkingSpots}
                             >
                                 <AiOutlinePlus
                                     className="text-primary"
-                                    size="lg"
                                 />
                             </button>
 
@@ -122,7 +151,7 @@ const AdminDashboard = () => {
                             </a>
 
                             <button
-                                className="btn btn-circle p-2"
+                                className="btn btn-circle text-3xl"
                                 onClick={() =>
                                     numParkingSpotsAvailable > 0 &&
                                     decParkingSpots()
@@ -130,7 +159,6 @@ const AdminDashboard = () => {
                             >
                                 <AiOutlineMinus
                                     className="text-primary"
-                                    size="lg"
                                 />
                             </button>
                         </div>
