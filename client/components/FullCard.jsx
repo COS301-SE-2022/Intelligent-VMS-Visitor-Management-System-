e47dd73c7e72a52ee10de4b17051d304aa1465f7
Kyle Smith <sKorpion19091@gmail.com>
2022-05-27 17:59:10 +0200

    ✨ (client) Added full card component

---
+++ client/components/FullCard.jsx
@@ -0,0 +1,11 @@
+const FullCard = () => {
+    return (
+        <div className="card">
+            <div className="card-body">
+
+            </div>
+        </div>
+    );
+};
+
+export default FullCard;
