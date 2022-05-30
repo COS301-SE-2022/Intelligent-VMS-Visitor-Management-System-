d724c088cde767223ebf45870ed8f498aa67f9f2
Kyle Smith <sKorpion19091@gmail.com>
2022-05-30 22:55:11 +0200

    ✅ (backend) Fixed unit test dependency issues

--- backend/libs/resident/src/resident.service.spec.ts
+++ backend/libs/resident/src/resident.service.spec.ts
@@ -1,13 +1,14 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { QueryBus } from "@nestjs/cqrs";
 import { ResidentService } from "./resident.service";
+import { MailService } from "@vms/mail";
 
 describe("ResidentService", () => {
     let service: ResidentService;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            providers: [ResidentService, QueryBus],
+            providers: [ResidentService, QueryBus, MailService],
         }).compile();
 
         service = module.get<ResidentService>(ResidentService);
