cfb37847f83fe452e14beaf133dff0a80599221a
LarisaBothaTuks <u20522623@tuks.co.za>
2022-06-03 11:10:07 +0200

    closing svg

--- client/components/QRScanner.jsx
+++ client/components/QRScanner.jsx
@@ -25,7 +25,23 @@ const QrScanner = ({showCondition}) => {
 
     return (
         <div className="w-auto flex justify-center items-center">
+
                         <video className = "relative rounded-lg" ref = {videoRef} autoPlay={true} id="videoElement">
+                        <a href="#">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              className="absolute h-6 w-6 flex-shrink-0 stroke-current"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                            >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
+                              d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                            />
+                            </svg>
+                            </a>
                         </video>
                      
                     <QrReader className = "rounded-lg"
