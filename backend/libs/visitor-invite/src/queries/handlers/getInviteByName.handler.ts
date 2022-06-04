e410ab55bf94b674ff954314974b16c30fb4021f
Stefan van Schoor <stefja7125@gmail.com>
2022-06-05 01:23:26 +0200

    ✨ Added ability to search invites by name

---
+++ backend/libs/visitor-invite/src/queries/handlers/getInviteByName.handler.ts
@@ -0,0 +1,16 @@
+import { QueryHandler, IQueryHandler } from "@nestjs/cqrs";
+import { GetInviteByNameQuery } from "../impl/getInviteByName.query";
+import { Model } from "mongoose";
+import { InjectModel } from "@nestjs/mongoose";
+import { Invite, InviteDocument } from "../../schema/invite.schema";
+
+@QueryHandler(GetInviteByNameQuery)
+export class GetInviteQueryHandler implements IQueryHandler {
+    constructor(@InjectModel(Invite.name) private inviteModel: Model<InviteDocument>) {}
+
+    async execute(query: GetInviteByNameQuery) {
+        const { name } = query; 
+        const invite = await this.inviteModel.findOne({ name: name });
+        return invite;
+    }
+}
