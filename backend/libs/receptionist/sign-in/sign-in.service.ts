198131aa6c5b361a8c083b644127cbbcc9cfb495
Daniel-J-Burgess <84472524+Daniel-J-Burgess@users.noreply.github.com>
2022-06-06 00:17:13 +0200

    🐛 (Tray) fixed the generateTray function and the Tray ID type

--- backend/libs/receptionist/sign-in/sign-in.service.ts
+++ backend/libs/receptionist/sign-in/sign-in.service.ts
@@ -3,6 +3,7 @@ import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import { SignInInviteCommand } from '@vms/receptionist/commands/impl/signInInvite.command';
 import { VisitorInviteService } from '@vms/visitor-invite';
 import { generateTrayCommand } from '../src/commands/impl/Tray/generateTray.command';
+import { getNumberOfTraysQuery } from '@vms/receptionist/queries/impl/getNumberOfTrays.query';
 
 @Injectable()
 export class SignInService {
@@ -21,10 +22,21 @@ export class SignInService {
                 new SignInInviteCommand(invitationID,notes));
         }
 
+        async getNumberOfTrays(){
+            const amount = await this.queryBus.execute(
+                new getNumberOfTraysQuery()
+            )
+            if(amount)
+                return amount;
+            else
+                return 0;
+        }
+
         //TODO(Daniel)
-        async generateTray(trayID: boolean,inviteID: string,containsResidentID: boolean,containsVisitorID: boolean){
+        //tray ID is generated from the number of Trays
+        async generateTray(inviteID: string,containsResidentID: boolean,containsVisitorID: boolean){
             console.log("generating tray");
-            return this.commandBus.execute(new generateTrayCommand(trayID, inviteID, containsResidentID,containsVisitorID));
+            return this.commandBus.execute(new generateTrayCommand(await this.getNumberOfTrays(),inviteID, containsResidentID,containsVisitorID));
         }
         
 
--- backend/libs/receptionist/src/commands/impl/Tray/generateTray.command.ts
+++ backend/libs/receptionist/src/commands/impl/Tray/generateTray.command.ts
@@ -1,6 +1,6 @@
 export class generateTrayCommand {
     constructor(
-        public readonly trayID: boolean,
+        public readonly trayID: number,
         public readonly inviteID: string,
         public readonly containsResidentID: boolean,
         public readonly containsVisitorID: boolean
