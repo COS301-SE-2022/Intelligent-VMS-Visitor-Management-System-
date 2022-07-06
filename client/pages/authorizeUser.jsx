0b313433e1e51b2b1185092a984467beb9cc560b
Kyle Smith <sKorpion19091@gmail.com>
2022-07-06 16:04:20 +0200

    ✨ (client) Added delete user ability

--- client/pages/authorizeUser.jsx
+++ client/pages/authorizeUser.jsx
@@ -1,5 +1,6 @@
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import { useQuery, gql } from "@apollo/client";
+import { useQuery, gql, useApolloClient } from "@apollo/client";
 
 import Layout from "../components/Layout.jsx";
 import AuthCard from "../components/AuthCard.jsx";
@@ -7,24 +8,84 @@ import AuthCard from "../components/AuthCard.jsx";
 import useAuth from "../store/authStore";
 
 const AuthorizeUser = () => {
+    const router = useRouter();
     const token = useAuth((state) => state.decodedToken)();
+    const [residentData, setResidentData] = useState([]);
+    const [receptionistData, setReceptionistData] = useState([]);
+
+    const client = useApolloClient();
+
+    const deleteUserAccount = (email, type) => {
+        client
+            .mutate({
+                mutation: gql`
+                mutation {
+                    deleteUserAccount(email: "${email}") 
+                }
+            `,
+            })
+            .then((res) => {
+                if (res.data.deleteUserAccount === true) {
+                    if (type === "Receptionist") {
+                        setReceptionistData(
+                            receptionistData.filter(
+                                (data) => data.email !== email
+                            )
+                        );
+                    } else {
+                        setResidentData(
+                            residentData.filter((data) => data.email !== email)
+                        );
+                    }
+                } else {
+                    console.log("ERROR!");
+                }
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+    };
 
     const { loading, data, error } = useQuery(gql`
         query {
             getUnauthorizedUsers {
-                email,
+                email
                 permission
             }
         }
     `);
 
     useEffect(() => {
-        if(!loading && !error) {
+        if (!loading && !error) {
             console.log(data.getUnauthorizedUsers);
-        } else if(!loading && error){
+            const userData = data.getUnauthorizedUsers;
+            console.log(userData);
+            const newReceptionistData = [];
+            const newResidentData = [];
+            userData.forEach((data) => {
+                if (data.permission === -1 || data.permission === 1) {
+                    newReceptionistData.push({
+                        email: data.email,
+                        type: "Receptionist",
+                        authorized: data.permission === 1 ? true : false,
+                    });
+                } else {
+                    newResidentData.push({
+                        email: data.email,
+                        type: "Resident",
+                    });
+                }
+            });
+
+            setReceptionistData(newReceptionistData);
+            setResidentData(newResidentData);
+        } else if (!loading && error) {
             console.error(error);
+            if(error.message === "Unauthorized") {
+                router.push("/expire");
+            }
         }
-    }, [loading,error]);
+    }, [loading, error, data]);
 
     return (
         <Layout>
@@ -32,19 +93,66 @@ const AuthorizeUser = () => {
                 <h1 className="mt-3 text-lg font-bold md:text-xl lg:text-3xl">
                     <span className="text-secondary">Authorize</span> User
                 </h1>
-                <div className="divider text-base md:text-lg lg:text-2xl">Receptionists</div>
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                    <AuthCard email="email@mail.com" type="Receptionist"/>
-                    <AuthCard email="email@mail.com" type="Receptionist"/>
-                    <AuthCard email="email@mail.com" authorized type="Receptionist"/>
-                </div>
-                <div className="divider text-base md:text-lg lg:text-2xl">Resident</div>
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                    <AuthCard email="email@mail.com" type="Resident"/>
-                    <AuthCard email="skorpion19091@gmail.com" type="Resident"/>
-                    <AuthCard email="email@mail.com" authorized type="Resident"/>
-                    <AuthCard email="email@mail.com" authorized type="Resident"/>
-                </div>
+                {receptionistData.length > 0 ? (
+                    <div>
+                        <div className="divider text-base md:text-lg lg:text-2xl">
+                            Receptionists
+                        </div>
+                        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+                            {receptionistData.map((entry, idx) => {
+                                return (
+                                    <AuthCard
+                                        key={idx}
+                                        email={entry.email}
+                                        type={entry.type}
+                                        authorized={entry.authorized}
+                                        deleteUserAccount={deleteUserAccount}
+                                        permission={token.permission}
+                                    />
+                                );
+                            })}
+                        </div>
+                    </div>
+                ) : (
+                    <div>
+                        {
+                        loading && 
+                            <progress className="progress progress-primary fixed left-[50%] top-[50%] w-56 translate-x-[-50%] translate-y-[-50%]">
+                                Loading
+                            </progress>
+                        }
+                    </div>
+                )}
+                {residentData.length > 0 ? (
+                    <div>
+                        <div className="divider text-base md:text-lg lg:text-2xl">
+                            Resident
+                        </div>
+                        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+                            {residentData.map((entry, idx) => {
+                                return (
+                                    <AuthCard
+                                        key={idx}
+                                        email={entry.email}
+                                        type={entry.type}
+                                        authorized={entry.authorized}
+                                        deleteUserAccount={deleteUserAccount}
+                                        permission={token.permission}
+                                    />
+                                );
+                            })}
+                        </div>
+                    </div>
+                ) : (
+                    <div>
+                        {
+                        loading && 
+                            <progress className="progress progress-primary fixed left-[50%] top-[50%] w-56 translate-x-[-50%] translate-y-[-50%]">
+                                Loading
+                            </progress>
+                        }
+                    </div>
+                )}
             </div>
         </Layout>
     );
