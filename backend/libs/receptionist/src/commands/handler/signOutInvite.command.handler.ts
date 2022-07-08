028769b7ce6f5c268d898a59868c220de31678e6
Tabby709 <u19294418@tuks.co.za>
2022-07-08 09:33:11 +0200

    👔(backend): removed code

--- backend/libs/receptionist/src/commands/handler/signOutInvite.command.handler.ts
+++ backend/libs/receptionist/src/commands/handler/signOutInvite.command.handler.ts
@@ -17,6 +17,9 @@ export class SignOutInviteCommandHandler implements ICommandHandler<SignOutInvit
         await this.trayModel.findOneAndUpdate({ inviteID: inviteId }, { containsVisitorID: false, containsResidentID: false });
 
         return await this.inviteModel.findOneAndUpdate({ inviteID: inviteId }, { inviteState: "signedOut", signOutTime: signOutTime });
+
+       return await this.inviteModel.findOneAndUpdate({ inviteID: inviteId }, { inviteState: "inActive", signOutTime });
+
         
     }
 }
