4defbdd5ac4159fb2934ca1869c7e53222c5d9d0
Stefan van Schoor <stefja7125@gmail.com>
2022-07-13 17:33:58 +0200

    ✨ (visitor modal) started implementing the modal code

--- client/components/VisitInfoModal.jsx
+++ client/components/VisitInfoModal.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useRef, useState } from "react";
 
-const VisitInfoModal = ({name,tray,}) => {
+const VisitInfoModal = ({setShowScanner}) => {
 
     return (
     <div className="relative flex-col justify-center items-center text-center">
-        <label>{name}</label>
-        <lable>{tray}</lable>   
+        <label>HOS</label>
+        <lable>TOKKELOSH</lable>   
     </div>    
     );
 };
