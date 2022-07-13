9232334b6c2fc5a084166c63d78d71cc2024e940
Stefan van Schoor <stefja7125@gmail.com>
2022-07-13 16:11:12 +0200

    ✨ (Visitor modal) Added neccesary data to queries

--- client/pages/receptionistDashboard.jsx
+++ client/pages/receptionistDashboard.jsx
@@ -65,7 +65,7 @@ const ReceptionistDashboard = () => {
                         idNumber
                         visitorName
                         inviteState
-                  
+                        requiresParking
                     }
                 }
             `,},
