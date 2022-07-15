b5d5a683f56e1f511c4ff6324d5b4fe81624586f
Stefan van Schoor <stefja7125@gmail.com>
2022-07-15 14:38:52 +0200

    ⚡️ (visitor modal) set up table interface

--- client/components/VisitInfoModal.jsx
+++ client/components/VisitInfoModal.jsx
@@ -1,24 +1,46 @@
 import React, { useEffect, useRef, useState } from "react";
 
-const VisitInfoModal = ({setShowInfo,visitModalData}) => {
+const VisitInfoModal = ({ setShowInfo, visitModalData }) => {
 
 
 
     useEffect(() => {
         // Stop video on component unmount
-    
+
     }, []);
     return (
-    <div className="relative flex-col justify-center items-center text-center" >
-        <label>{visitModalData.visitorName}</label>
-        <label>{visitModalData.idDocType}</label>
-        <label>{visitModalData.idNumber}</label>
-        <label>{visitModalData.userEmail}</label>
-        <label>{visitModalData.inviteDate}</label>
-        <label>{visitModalData.inviteID}</label>  
+        <div className="relative flex-col justify-center items-center text-center" >
+            <label>{visitModalData.visitorName}</label>
+            <label>{visitModalData.idDocType}</label>
+            <label>{visitModalData.idNumber}</label>
+            <label>{visitModalData.userEmail}</label>
+            <label>{visitModalData.inviteDate}</label>
+            <label>{visitModalData.inviteID}</label>
 
-        
-    </div>   
+            <table className="mb-5 table w-full">
+                <thead>
+                    <tr>
+                        <th>Visitor Name</th>
+                        <th>Document type</th>
+                        <th>Visitor ID</th>
+                        <th>Invite date</th>
+                        <th>Host email</th>
+                        <th>Invite ID</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr className="hover" key={idx}  >
+                        <th ></th>
+                        <td></td>
+                        <td ></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+
+
+        </div>
     );
 };
 
