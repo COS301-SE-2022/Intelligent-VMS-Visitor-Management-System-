f83d074b8389c27c6a295b1f065654429e5821cb
Stefan van Schoor <stefja7125@gmail.com>
2022-07-21 17:38:17 +0200

    ✅ (integration tests) added visibility check for resend email button

--- client/cypress/integration/verify.spec.js
+++ client/cypress/integration/verify.spec.js
@@ -18,7 +18,7 @@ describe('Verify Page', () => {
         //Enter sign up info
         describe('Login as receptionist', () => {
             cy.url().should('include', 'signUp');//confirm correct page
-            cy.get('input[name="email"]').type("e@mail.com").should('have.value', 'e@mail.com');
+            cy.get('input[name="email"]').type("h@mail.com").should('have.value', 'h@mail.com');
             cy.get('input[name="name"]').type("Stefan").should('have.value', 'Stefan');
             cy.get('input[name="password"]').type("P@ssword1").should('have.value', 'P@ssword1');
             cy.get('input[name="confirmPassword"]').type("P@ssword1").should('have.value', 'P@ssword1');
@@ -31,6 +31,8 @@ describe('Verify Page', () => {
             cy.wait(1000);
             cy.url().should('include', 'verify');
             cy.contains('Please check your email to verify your account');//confirm correct page
+            // cy.contains('RESEND EMAIL');//confirm correct page
+            cy.get('.btn-primary ').should('be.visible');
         })
 
     })
