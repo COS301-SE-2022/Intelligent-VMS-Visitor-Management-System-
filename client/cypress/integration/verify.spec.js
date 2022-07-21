11ccb81723361f9e996f647e08abd50eb7a35710
Stefan van Schoor <stefja7125@gmail.com>
2022-07-21 17:32:27 +0200

    ✅ (integration tests) updated email info to remove clashes

---
+++ client/cypress/integration/verify.spec.js
@@ -0,0 +1,38 @@
+describe('Verify Page', () => {
+    it('Visits the Kitchen Sink', () => {
+        //Navigate to Home page
+        describe('Navigate to Home page/confirm', () => {
+            cy.visit('https://vms-client.vercel.app/');
+            cy.wait(500);
+            cy.contains('Go Beyond The Lobby');//confirm correct page
+        })
+
+        //Navigate to signup page
+        describe('Navigate to sign up page', () => {
+            cy.wait(500);
+            cy.get('.menuIcon').click();
+            cy.wait(500);
+            cy.contains('Signup').click();
+        })
+
+        //Enter sign up info
+        describe('Login as receptionist', () => {
+            cy.url().should('include', 'signUp');//confirm correct page
+            cy.get('input[name="email"]').type("e@mail.com").should('have.value', 'e@mail.com');
+            cy.get('input[name="name"]').type("Stefan").should('have.value', 'Stefan');
+            cy.get('input[name="password"]').type("P@ssword1").should('have.value', 'P@ssword1');
+            cy.get('input[name="confirmPassword"]').type("P@ssword1").should('have.value', 'P@ssword1');
+            cy.get('input[name="idNumber"]').type("0105085368078").should('have.value', '0105085368078');
+            cy.get('input[name="userType"]').first().check();
+            cy.get('.btn-primary ').click();
+        })
+
+        describe('confirm page movement', () => {
+            cy.wait(1000);
+            cy.url().should('include', 'verify');
+            cy.contains('Please check your email to verify your account');//confirm correct page
+        })
+
+    })
+
+})
