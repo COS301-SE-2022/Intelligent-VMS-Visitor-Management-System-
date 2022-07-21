cc8008c85b18424267f8f821aaf9370f1c86d58b
Daniel-J-Burgess <84472524+Daniel-J-Burgess@users.noreply.github.com>
2022-07-21 13:49:52 +0200

    ✅ (integration) fixing some errors

--- client/cypress/integration/receptionistDashBoard.spec.js
+++ client/cypress/integration/receptionistDashBoard.spec.js
@@ -6,9 +6,9 @@ describe('My First Test', () => {
       cy.contains('Go Beyond The Lobby');//confirm correct page
 
     //Navigate to login page
-      //cy.wait(500);
+      cy.wait(500);
       cy.get('.menuIcon').click();
-      //cy.wait(500);
+      cy.wait(500);
       cy.contains('Login').click();
 
     //Login as receptionist
@@ -18,8 +18,10 @@ describe('My First Test', () => {
       cy.get('.btn-primary ').click();
 
     //========================Signed in as Receptionist=================================
-   
-      cy.url().should('include', '/createInvite');//confirm correct page
+        
+    cy.location('pathname', {timeout: 60000}).should('include', '/createInvite');//waiting for backend to wake up.
+
+      //cy.url().should('include', '/createInvite');//confirm correct page
       cy.get('.menuIcon').click();
       cy.get('.dropdown-content').children().should('contain','Create Invite')//check for corrct menu options.
       .and('contain','Your Dashboard')
@@ -31,6 +33,25 @@ describe('My First Test', () => {
     
       //Checking visitor dashboard
       cy.url().should('include', '/visitorDashboard');//confirm correct page
+      cy.contains('Welcome back,Receptionist');
+      cy.contains('User Invites');
+      cy.contains('Total Number Of Invites Sent');
+      cy.contains('Maximum Invites Allowed');
+    
+
+      /*cy.get('header').then(($a) => { 
+        if ($a.text().includes('Account')) {
+            cy.contains('Account')
+            .click({force:true})
+        } else if ($a.text().includes('Sign')) { 
+            cy.contains('Sign In')
+            .click({force:true})  
+        } else {
+            cy.get('.navUser-item--account .navUser-action').click({force:true})
+        }
+       
+    }) */
+      
     })
   })
   
