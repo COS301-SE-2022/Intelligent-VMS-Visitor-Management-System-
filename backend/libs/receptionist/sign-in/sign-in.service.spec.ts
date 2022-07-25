dd1de2a136208e7759bc1e98a12cfa741f21f718
Tabby709 <u19294418@tuks.co.za>
2022-07-25 13:35:50 +0200

    🔥(backend): removed a test

--- backend/libs/receptionist/sign-in/sign-in.service.spec.ts
+++ backend/libs/receptionist/sign-in/sign-in.service.spec.ts
@@ -198,7 +198,7 @@ describe('SignInService', () => {
    
   describe("generateTrayID",()=>{
     it("should return a valid Tray id", async()=>{
-      //const generateTrayID=await service.generateTrayID();
+      
       
 
     });
