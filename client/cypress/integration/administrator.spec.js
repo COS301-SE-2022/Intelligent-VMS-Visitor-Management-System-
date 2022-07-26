7b6fd37b89bcb92975e9168121f7c085203966dc
Daniel-J-Burgess <84472524+Daniel-J-Burgess@users.noreply.github.com>
2022-07-26 15:31:49 +0200

    ✅ (Integration) reworked variable capturing

--- client/cypress/integration/administrator.spec.js
+++ client/cypress/integration/administrator.spec.js
@@ -1,42 +1,42 @@
-var myObj = { inText: 0,numberAllowed: 0,numberSent: 0 };
+var myObj = { inText: 0, numberAllowed: 0, numberSent: 0 };
 describe('Receptionist tests', () => {
     it('tests various administrator functions', () => {
         var today = new Date();
         var dd = String(today.getDate()).padStart(2, '0');
         var mm = String(today.getMonth() + 1).padStart(2, '0');
         var yyyy = today.getFullYear();
-        today =yyyy + '-' + mm + '-' + dd ;
+        today = yyyy + '-' + mm + '-' + dd;
 
         describe('Navigate to Home page/confirm', () => {
             cy.visit('https://vms-client.vercel.app/');
             cy.contains('Go Beyond The Lobby');//confirm correct page
         })
-    
+
         describe('Navigate to login page/confirm', () => {
             cy.get('.menuIcon').click();//misses it sometimes if it is not clicked twice
-            cy.wait(1000);
+            //cy.wait(1000);
             cy.get('.menuIcon').click();//do not remove redundancy without running it a few times to test
-            cy.contains('Login', {timeout: 7000}).click();
+            cy.contains('Login', { timeout: 7000 }).click({ force: true });
         })
 
         describe('Login as administrator', () => {
             cy.url().should('include', 'login');//confirm correct page
-            cy.get('input[name="email"]').type("admin@mail.com" ).should('have.value', 'admin@mail.com');
-            cy.get('input[name="password"]').type("password" ).should('have.value', 'password');
+            cy.get('input[name="email"]').type("admin@mail.com").should('have.value', 'admin@mail.com');
+            cy.get('input[name="password"]').type("password").should('have.value', 'password');
             cy.get('.btn-primary ').click();
         })
 
         describe('confirm signed-in and  check for elements', () => {
-            cy.location('pathname', {timeout: 60000}).should('include', '/createInvite');//waiting for backend to wake up.
+            cy.location('pathname', { timeout: 60000 }).should('include', '/createInvite');//waiting for backend to wake up.
             cy.get('.menuIcon').click();
-            cy.get('.dropdown-content').children().should('contain','Create Invite')//check for correct menu options.
-            .and('contain','Your Dashboard')
-            .and('contain','Manage Users')
-            .and('contain','Admin Dashboard')
-            .and('contain','Logout');
+            cy.get('.dropdown-content').children().should('contain', 'Create Invite')//check for correct menu options.
+                .and('contain', 'Your Dashboard')
+                .and('contain', 'Manage Users')
+                .and('contain', 'Admin Dashboard')
+                .and('contain', 'Logout');
             cy.get('.menuIcon').click();
         })
-        
+
         describe('Filling in a user invite', () => {
             cy.get('input[name="email"]').type('Stefan1234@mail.com').should('have.value', 'Stefan1234@mail.com');
             cy.get('input[name="idValue"]').type('9910304129088').should('have.value', '9910304129088');
@@ -44,96 +44,99 @@ describe('Receptionist tests', () => {
             cy.get('input[name="visitDate"]').type(today).should('have.value', today);
             cy.get('button[type="submit"]').click();
         })
-        // let inText = 0;
-        // let numberAllowed= 0;
-        // let numberSent= 0;
-       
 
         describe('Checking max invites/radial percentage', () => {
-            cy.location('pathname', {timeout: 60000}).should('include', '/visitorDashboard');//waiting for backend to wake up.
+            cy.location('pathname', { timeout: 60000 }).should('include', '/visitorDashboard');//waiting for backend to wake up.
             cy.wait(2000)
-         
+
             cy.contains('p', 'You are allowed to send ')      //find Stefans column
-            .parent().then(($span) => {
+                .parent().then(($span) => {
 
-                // $span is the object that the previous command yielded
-                myObj.inText = $span.text();
-                myObj.numberAllowed=myObj.inText.slice(-10, -9);
-                myObj.numberSent= myObj.inText.slice(19, 20);
-               
-                
-                cy.log(Math.trunc((myObj.numberSent/myObj.numberAllowed)*100));  
-                cy.get('div[class="radial-progress text-base-content"]').within(($tr)=>{                        //search only within the row
-                    cy.contains(Math.trunc((myObj.numberSent/myObj.numberAllowed)*100));
-                })  
-                cy.log(myObj.numberSent); 
+                    myObj.inText = $span.text();
+                    let temp= myObj.inText.search("send");
+                    temp = parseInt(temp);
+                    myObj.numberAllowed = myObj.inText.slice(temp+5,temp+7);
+                    myObj.numberSent = myObj.inText.slice(19, 21);
+                     myObj.numberAllowed = parseInt(myObj.numberAllowed);
+                     myObj.numberSent = parseInt(myObj.numberSent);
+                   
+                    // cy.log(Math.trunc(myObj.numberAllowed));
+                    // cy.log(Math.trunc((myObj.numberSent / myObj.numberAllowed) * 100));
+                     cy.get('div[class="radial-progress text-base-content"]').within(($tr) => {                        //search only within the row
+                        cy.contains(Math.trunc((myObj.numberSent / myObj.numberAllowed) * 100));
+                     })
 
-                describe('canceling the personal invite', () => {
-                    cy.contains('td', 'Stefan1234@mail.com')//find stafans column
-                    .parent()                               //his row
-                    .within(($tr)=>{                        //search only within the row
-                        cy.get('td button').click()
-                    })  
+                    describe('canceling the personal invite', () => {
+                        cy.contains('td', 'Stefan1234@mail.com')//find stafans column
+                            .parent()                               //his row
+                            .within(($tr) => {                        //search only within the row
+                                cy.get('td button').click()
+                            })
+                    })
+
+                    describe('confirming that the Invites sent has correctly updated', () => {
+                        cy.wait(3000);                                      //wait for page information to be updated
+                        cy.contains('p', 'You are allowed to send ')      //find Stefans column
+                            .parent().then(($span) => {
+                                let myText = $span.text();
+                                let newSent = myText.slice(19, 21);
+                                if (newSent != (myObj.numberSent - 1)) {
+                                    throw new Error("Error, 'sent invites' value did not update correctly update after dismissing the invite");
+                                }
+                            })
+                    })
                 })
 
-                describe('confirming that the Invites sent has correctly updated', () => {
-                    cy.wait(2000);//wait for page information to be updated
-                    cy.contains('p', 'You are allowed to send ')      //find Stefans column
-                        .parent().then(($span) => {
-                            let myText = $span.text();
-                            let newSent= myText.slice(19, 20);
-                            if (newSent!=(myObj.numberSent-1)) {
-                                throw new Error("Error, 'sent invites' value did not update correctly update after dismissing the invite");
-                            }
-                        })
-                })
-            })
-           
-        
+
         })
         describe('Navigate to admin dashboard/confirm navigation success', () => {
-            // cy.log('hmm');  
-            // cy.wait(6000);
             cy.get('.menuIcon').click();
-            cy.contains('Admin Dashboard', { matchCase: false }).click();
+            cy.contains('Admin Dashboard', { matchCase: false }).click({ force: true });
             cy.url().should('include', '/adminDashboard');//confirm correct page
         })
         describe('set new visitor limit and confirm it correctly changes', () => {
             cy.wait(4000)
             cy.get('p[ id="numInvitesPerResident"]').parent().then(($span) => {
-                myObj.inText = $span.text();   
-                myObj.numberAllowed=myObj.inText.slice(-10, -9);
-                myObj.numberSent= myObj.inText.slice(19, 20);
+                myObj.inText = $span.text();
+                let temp= myObj.inText.search("send");
+                temp = parseInt(temp);
+                myObj.numberAllowed = myObj.inText.slice(temp+5,temp+7);
+                myObj.numberSent = myObj.inText.slice(19, 21);
                 cy.log(myObj.inText);
-                 cy.get('button[data-testid="increaseInvites"]').click();
-                 cy.get('button[class="btn btn-primary btn-sm space-x-3 lg:btn-md"]').click();
+                cy.get('button[data-testid="increaseInvites"]').click();
+                cy.get('button[class="btn btn-primary btn-sm space-x-3 lg:btn-md"]').click();
 
                 describe('Navigate to visitor dashboard/confirm navigation success', () => {
                     cy.get('.menuIcon').click();
-                    cy.contains('your Dashboard', { matchCase: false }).click();
+                    cy.contains('your Dashboard', { matchCase: false }).click({ force: true });
                     cy.url().should('include', '/visitorDashboard');//confirm correct page
                 })
-                cy.wait(2000);//wait for page information to be updated
-                cy.contains('p', 'You are allowed to send ')      //find Stefans column
+                cy.wait(3000);                                      //wait for page information to be updated
+                cy.contains('p', 'You are allowed to send ')        //find Stefans column
                     .parent().then(($span) => {
                         let myText = $span.text();
-                       let myallowed=myText.slice(-10, -9);
-                       cy.log(myallowed);
-                       cy.log(myObj.inText);
-                       myObj.inText=parseInt(myObj.inText);
-                       myallowed=parseInt(myallowed);
-                       //cy.log(myallowed+1);
-                        if (myallowed!=(myObj.inText+1)) {
-                            //cy.log("hmmmm");
+                        let temp=myText.search("send");
+                        temp = parseInt(temp);
+                        let myallowed =myText.slice(temp+5,temp+7);
+                        cy.log(myallowed);
+                        cy.log(myObj.inText);
+                        myObj.inText = parseInt(myObj.inText);
+                        myallowed = parseInt(myallowed);
+                        if (myallowed != (myObj.inText + 1)) {
                             throw new Error("Error, 'number of allowed invites does not update correctly");
                         }
                     })
             })
-
-                 
         })
-
-       
+        describe('resetting the allowed invites', () => {
+            cy.get('.menuIcon').click();
+            cy.contains('admin dashboard', { matchCase: false }).click({ force: true });
+            cy.url().should('include', '/adminDashboard');      //confirm correct page
+            cy.wait(3000);
+            cy.get('button[data-testid="decreaseInvites"]').click();
+            //cy.wait(5000);
+            cy.get('button[class="btn btn-primary btn-sm space-x-3 lg:btn-md"]').click();
+        })
 
     })
 })
--- client/cypress/integration/receptionist.spec.js
+++ client/cypress/integration/receptionist.spec.js
@@ -4,42 +4,42 @@ describe('Receptionist tests', () => {
         var dd = String(today.getDate()).padStart(2, '0');
         var mm = String(today.getMonth() + 1).padStart(2, '0');
         var yyyy = today.getFullYear();
-        today =yyyy + '-' + mm + '-' + dd ;
+        today = yyyy + '-' + mm + '-' + dd;
 
         describe('Navigate to Home page/confirm', () => {
             cy.visit('https://vms-client.vercel.app/');
             cy.contains('Go Beyond The Lobby');//confirm correct page
-         })
+        })
 
         describe('Navigate to login page/confirm', () => {
             cy.get('.menuIcon').click();//misses it sometimes if it is not clicked twice
-            cy.wait(1000);
+            // cy.wait(1000);
             cy.get('.menuIcon').click();//do not remove redundancy without running it a few times to test
-        
-            cy.contains('Login', {timeout: 7000}).click();
+
+            cy.contains('Login', { timeout: 7000 }).click({ force: true });
         })
 
         describe('Login as receptionist', () => {
             cy.url().should('include', 'login');//confirm correct page
-            cy.get('input[name="email"]').type("receptionist@mail.com" ).should('have.value', 'receptionist@mail.com');
-            cy.get('input[name="password"]').type("password" ).should('have.value', 'password');
+            cy.get('input[name="email"]').type("receptionist@mail.com").should('have.value', 'receptionist@mail.com');
+            cy.get('input[name="password"]').type("password").should('have.value', 'password');
             cy.get('.btn-primary ').click();
         })
 
         //========================Signed in as Receptionist=================================
-            
+
         describe('confirm signed-in and  check for elements', () => {
-            cy.location('pathname', {timeout: 60000}).should('include', '/createInvite');//waiting for backend to wake up.
+            cy.location('pathname', { timeout: 60000 }).should('include', '/createInvite');//waiting for backend to wake up.
             cy.get('.menuIcon').click();
-            cy.get('.dropdown-content').children().should('contain','Create Invite')//check for correct menu options.
-            .and('contain','Your Dashboard')
-            .and('contain','Manage Residents')
-            .and('contain','Receptionist Dashboard')
-            .and('contain','Logout');
+            cy.get('.dropdown-content').children().should('contain', 'Create Invite')//check for correct menu options.
+                .and('contain', 'Your Dashboard')
+                .and('contain', 'Manage Residents')
+                .and('contain', 'Receptionist Dashboard')
+                .and('contain', 'Logout');
             cy.get('.menuIcon').click();
-            cy.contains('Your Dashboard').click();
+            cy.contains('Your Dashboard').click({ force: true });
         })
-        
+
         describe('Navigate to visitor dashboard/confirm navigation success', () => {
             cy.url().should('include', '/visitorDashboard');//confirm correct page
         })
@@ -53,31 +53,23 @@ describe('Receptionist tests', () => {
             cy.contains('Open invites', { matchCase: false });
 
             //check for table/correct table headings.
-            cy.get('.table-compact').children().should('contain','Email')
-            .and('contain','ID Document Type')
-            .and('contain','ID Number')
-            .and('contain','Date')
-            .and('contain','Cancel Invite');
+            cy.get('.table-compact').children().should('contain', 'Email')
+                .and('contain', 'ID Document Type')
+                .and('contain', 'ID Number')
+                .and('contain', 'Date')
+                .and('contain', 'Cancel Invite');
 
             //check that the footer has rendered
-            cy.contains('Team Firestorm').and('contain','Providing reliable tech since 2022');
+            cy.contains('Team Firestorm').and('contain', 'Providing reliable tech since 2022');
         })
-       
+
         describe('Confirm receptionist', () => {
 
         })
-            
-        //uncomment the 5 lines of code below to have it download a pdf as well
-        /*
-        describe("will click the download button",()=>{
-            cy.get('[download="Receptionist-weekly.png"]')
-            .should('be.visible')
-            .click()
-        })*/
 
         describe('Navigate to receptionist invite user/confirm navigation success', () => {
             cy.get('.menuIcon').click();
-            cy.contains('create invite', { matchCase: false }).click();
+            cy.contains('create invite', { matchCase: false }).click({ force: true });
             cy.url().should('include', '/createInvite');//confirm correct page
         })
 
@@ -90,20 +82,20 @@ describe('Receptionist tests', () => {
         })
 
         describe('checking that invite correctly displays on visitor dashboard', () => {
-           cy.get('table[class="table-compact m-2 mb-5 table w-full md:table-normal"]', {timeout: 8000})
-           .children()
-           .should('contain','Stefan1234@mail.com')
-           .should('contain','9910304129088')
-           .should('contain',today)
-           .should('contain','RSA-ID');
+            cy.get('table[class="table-compact m-2 mb-5 table w-full md:table-normal"]', { timeout: 8000 })
+                .children()
+                .should('contain', 'Stefan1234@mail.com')
+                .should('contain', '9910304129088')
+                .should('contain', today)
+                .should('contain', 'RSA-ID');
         })
 
         describe('canceling a personal invite', () => {
             cy.contains('td', 'Stefan1234@mail.com')//find stafans column
-            .parent()                               //his row
-            .within(($tr)=>{                        //search only within the row
-                cy.get('td button').click()
-            })  
+                .parent()                               //his row
+                .within(($tr) => {                        //search only within the row
+                    cy.get('td button').click()
+                })
         })
 
         describe('Navigate to receptionist invite user/confirm navigation success', () => {
@@ -121,10 +113,10 @@ describe('Receptionist tests', () => {
         })
 
         describe('receptionistDashboard/confirm navigation success', () => {
-            cy.contains('Open Invites', {timeout: 8000})
-            cy.get('.menuIcon').click();
-            cy.get('.menuIcon').click();
-            cy.contains('Receptionist Dashboard', { matchCase: false }).click();
+            cy.contains('Open Invites', { timeout: 8000 })
+            cy.get('.menuIcon').click({ force: true });
+            // cy.get('.menuIcon').click();
+            cy.contains('Receptionist Dashboard', { matchCase: false }).click({ force: true });
             cy.url().should('include', '/receptionistDashboard');//confirm correct page
         })
 
@@ -134,36 +126,35 @@ describe('Receptionist tests', () => {
             cy.contains('Bulk-SignIn');
             cy.contains('Scan to Search');
             cy.get('table[class="mb-5 table w-full"]')//Confirm table has loaded
-            .children()
-            .should('contain','Visitor Name')
-            .should('contain','Visitor ID')
-            .should('contain','Status')
+                .children()
+                .should('contain', 'Visitor Name')
+                .should('contain', 'Visitor ID')
+                .should('contain', 'Status')
         })
 
         describe('sign in a visitor', () => {
             cy.contains('td', '9910304129088')      //find Stefans column
-            .parent()                               //his row
-            .within(($tr)=>{                        //search only within the row
-                cy.get('td label').click()
-            })  
+                .parent()                               //his row
+                .within(($tr) => {                        //search only within the row
+                    cy.get('td label').click()
+                })
             cy.contains('Confirm sign-in of visitor with id');
 
-            cy.get('div[class="modal-box"]').within(($div)=>{
+            cy.get('div[class="modal-box"]').within(($div) => {
                 cy.contains('Sign in').click();
             });
         })
         describe('sign out the visitor that was just signed in', () => {
-           
+
             cy.wait(4000);
             cy.contains('td', '9910304129088')      //find Stefans column
-            .parent()                               //his row
-            .within(($tr)=>{                        //search only within the row
-                cy.get('td label').click()
-            })  
-            cy.get('div[class="modal-box"]').within(($div)=>{
+                .parent()                               //his row
+                .within(($tr) => {                        //search only within the row
+                    cy.get('td label').click()
+                })
+            cy.get('div[class="modal-box"]').within(($div) => {
                 cy.contains('Sign out').click();
             });
         })
     })
-  })
-  
+})
