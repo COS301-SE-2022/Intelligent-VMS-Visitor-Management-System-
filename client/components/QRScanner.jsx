7a67eda6dc42621968b89c6170db44569fd2565f
Daniel-J-Burgess <84472524+Daniel-J-Burgess@users.noreply.github.com>
2022-08-13 19:39:00 +0200

    🐛 (QRScanner) fixing class designation

--- client/components/QRScanner.jsx
+++ client/components/QRScanner.jsx
@@ -139,9 +139,9 @@ const QRScanner = ({
                 </div>
             )}
 
-      <div class="flex mt-3 justify-center">
-        <span class="fill-current text-error w-6 h-full align-middle fill-bg-error"><TiWarning size="lg" color="bg-error"/></span>
-        <p class="ml-2 font-bold text-error">Ensure that QR Code is visible</p>
+      <div className="flex mt-3 justify-center">
+        <span className="fill-current text-error w-6 h-full align-middle fill-bg-error"><TiWarning size="lg" color="bg-error"/></span>
+        <p className="ml-2 font-bold text-error">Ensure that QR Code is visible</p>
       </div>
         </div>
     );
