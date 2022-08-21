435831522d92608201554a3070940cdbd7f0702b
Stefan van Schoor <stefja7125@gmail.com>
2022-08-21 17:16:13 +0200

    💡 (curfew) added explaining comments

--- backend/libs/restrictions/src/commands/handlers/setCurfewTime.handler.ts
+++ backend/libs/restrictions/src/commands/handlers/setCurfewTime.handler.ts
@@ -3,9 +3,9 @@ import { Model } from "mongoose";
 import { InjectModel } from "@nestjs/mongoose";
 import { Restriction, RestrictionDocument } from "../../schema/restriction.schema";
 import { SetCurfewTimeCommand } from "../impl/setCurfewTime.command";
-
+//curfew time wherafter the invites are extended
 @CommandHandler(SetCurfewTimeCommand)
-export class SetNumInvitesCommandHandler implements ICommandHandler {
+export class SetCurfewTimeHandler implements ICommandHandler {
     constructor(
         @InjectModel(Restriction.name) private restrictionModel: Model<RestrictionDocument>,
     ) {
