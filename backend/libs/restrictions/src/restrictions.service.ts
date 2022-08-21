a01fc3fa4e31229a2521954865b7c168db8fbc6e
Stefan van Schoor <stefja7125@gmail.com>
2022-08-21 17:39:21 +0200

    ✨ (curfew) added needed imports in service file

--- backend/libs/restrictions/src/restrictions.service.ts
+++ backend/libs/restrictions/src/restrictions.service.ts
@@ -3,6 +3,8 @@ import { QueryBus, CommandBus } from "@nestjs/cqrs";
 
 import { SetNumInvitesCommand } from "./commands/impl/setNumInvites.command";
 import { GetNumInvitesQuery } from './queries/impl/getNumInvites.query';
+import { SetCurfewTimeCommand } from "./commands/impl/setCurfewTime.command";
+import { GetCurfewTimeQuery } from './queries/impl/getCurfewTime.query';
 
 @Injectable()
 export class RestrictionsService {
@@ -16,5 +18,15 @@ export class RestrictionsService {
     // Set the number of invites a resident may have sent/open at once
     async getNumInvitesPerResident() {
         return await this.queryBus.execute(new GetNumInvitesQuery());
+    }
+
+    // Set the number of invites a resident may have sent/open at once
+    async setCurfewTime(numInvites: number) {
+        return this.commandBus.execute(new SetCurfewTimeCommand(numInvites));
+    }
+
+    // Set the number of invites a resident may have sent/open at once
+    async getCurfewTime() {
+        return await this.queryBus.execute(new GetCurfewTimeQuery());
     }
 }
