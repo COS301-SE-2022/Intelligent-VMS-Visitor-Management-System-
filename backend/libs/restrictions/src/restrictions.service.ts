e58ad81b62f741e408a386a3427512f897db1dd6
Tabby709 <u19294418@tuks.co.za>
2022-08-26 06:45:53 +0200

    ➕ (backend): added an import

--- backend/libs/restrictions/src/restrictions.service.ts
+++ backend/libs/restrictions/src/restrictions.service.ts
@@ -5,6 +5,7 @@ import { SetNumInvitesCommand } from "./commands/impl/setNumInvites.command";
 import { GetNumInvitesQuery } from './queries/impl/getNumInvites.query';
 import { SetCurfewTimeCommand } from "./commands/impl/setCurfewTime.command";
 import { GetCurfewTimeQuery } from './queries/impl/getCurfewTime.query';
+import { Cron, CronExpression } from '@nestjs/schedule';
 
 @Injectable()
 export class RestrictionsService {
