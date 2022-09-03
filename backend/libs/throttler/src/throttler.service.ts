d5a849064e9e099c3e084afaa9e3c65aeee701ff
Kyle Smith <sKorpion19091@gmail.com>
2022-09-03 15:15:38 +0200

    (backend) Added interfaces, guards and providers for rate limiter

--- backend/libs/throttler/src/throttler.service.ts
+++ backend/libs/throttler/src/throttler.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { RateLimitStorage } from './interfaces/ratelimit-storage.interface';
 
 @Injectable()
-export class ThrottlerService {}
+export class RateLimitStorageService implements RateLimitStorage {
+    private _storage: Record<string, number[]> = {};
+
+    get storage(): Record<string, number[]> {
+        return this._storage;
+    }
+
+    async getRecord(key: string): Promise<number[]> {
+        return this.storage[key] || [];
+    }
+}
