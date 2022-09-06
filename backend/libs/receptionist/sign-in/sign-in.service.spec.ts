0230ab4d1ee09241f96b9eff11e43f6e43975e7f
Tabby709 <u19294418@tuks.co.za>
2022-09-06 04:33:36 +0200

    ✅ (backend): added a new test case

--- backend/libs/receptionist/sign-in/sign-in.service.spec.ts
+++ backend/libs/receptionist/sign-in/sign-in.service.spec.ts
@@ -278,6 +278,10 @@ describe('SignInService', () => {
       jest.spyOn(inviteService, 'createInviteForBulkSignIn').mockReturnValueOnce(Promise.resolve('hello world'))
       jest.spyOn(commandBusMock as any, 'execute').mockReturnValueOnce([1, 2, 3, 4])
 
+      // Act
+      const response = await service.bulkSignIn('name.email', 'user.email@email.com');
+
+
 
 
 
