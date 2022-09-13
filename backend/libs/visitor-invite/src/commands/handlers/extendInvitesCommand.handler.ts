68794df6fc3e1dcedec5dd0baeab4fcb3f01d58c
LarisaBothaTuks <u20522623@tuks.co.za>
2022-09-13 11:36:40 +0200

    🗃️ (autoExtension): Add bulk cancellation command

--- backend/libs/visitor-invite/src/commands/handlers/extendInvitesCommand.handler.ts
+++ backend/libs/visitor-invite/src/commands/handlers/extendInvitesCommand.handler.ts
@@ -1,11 +1,11 @@
 import { CommandHandler, ICommandHandler } from "@nestjs/cqrs";
-import { ExtendInvitesCommand } from "../impl/extendInvite.command";
+import { ExtendInvitesCommand } from "../impl/extendInvites.command";
 import { Model } from "mongoose";
 import { InjectModel } from "@nestjs/mongoose";
 import { Invite, InviteDocument } from "../../schema/invite.schema";
 
 @CommandHandler(ExtendInvitesCommand)
-export class ExtendInviteCommandHandler implements ICommandHandler {
+export class ExtendInvitesCommandHandler implements ICommandHandler {
     constructor(
         @InjectModel(Invite.name) private inviteModel: Model<InviteDocument>,
     ) {}
