4af8f547e576da3bd5c2db6c472ca33a6a1648b4
Stefan van Schoor <stefja7125@gmail.com>
2022-09-23 15:13:43 +0200

    🐛 (initialize) fixed many bugs which made the page inaccesible

--- client/pages/initialize.jsx
+++ client/pages/initialize.jsx
@@ -3,22 +3,30 @@ import { useState, useEffect, useRef } from "react";
 import { gql, useQuery, useMutation, useApolloClient } from "@apollo/client";
 
 import Layout from "../components/Layout";
-import AdminCard from "../components/AdminCard";
-import LineChart from "../components/LineChart";
-import DownloadChart from "../components/DownloadChart";
-import VisitorSearchResults from "../components/VisitorSearchResults";
-
 import useDateRange from "../hooks/useDateRange.hook";
 import useAuth from "../store/authStore";
 
-import { AiOutlinePlus, AiOutlineMinus, AiOutlineCar } from "react-icons/ai";
-import { BiBuildingHouse, BiMailSend } from "react-icons/bi";
-import { FaSearch, FaCarSide, FaPeopleArrows } from "react-icons/fa";
-import { MdBlock, MdDataSaverOn, MdDataSaverOff, MdOutlineCancel, } from "react-icons/md";
+import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai";
+import {
+    MdBlock,
+    MdDataSaverOn,
+    MdDataSaverOff,
+} from "react-icons/md";
 
+// Returns string in format yyyy-mm-dd given Date Object
+const getFormattedDateString = (date) => {
+    if (date instanceof Date) {
+        const month = date.getMonth() + 1;
+        const day = date.getDate();
+        return [
+            date.getFullYear(),
+            (month > 9 ? "" : "0") + month,
+            (day > 9 ? "" : "0") + day,
+        ].join("-");
+    }
+};
 
 const initialize = () => {
-
     // NextJS Page Router
     const router = useRouter();
 
@@ -32,6 +40,29 @@ const initialize = () => {
     const mins = ["00","01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29"
     ,"30","31","32","33","34","35","36","37","38","39","40","41","42","43","44","45","46","47","48","49","50","51","52","53","54","55","56","57","58","59"];
 
+    // Visitor invite data object for chart
+    const [visitorVals, setVisitorVals] = useState({
+        data: [],
+        labels: [],
+        label: "Invites",
+    });
+
+    // Parking data object for chart
+    const [parkingVals, setParkingVals] = useState({
+        data: [],
+        labels: [],
+        label: "Parking",
+    });
+
+
+    // Parking Date Range Hook
+    const [
+        parkingStartDate,
+        parkingEndDate,
+        parkingDateMap,
+        setParkingDateMap,
+    ] = useDateRange(getFormattedDateString(new Date(Date.now())), 7);
+
 
     // Initial number of invites per resident for fallback
     const [initialNumInvitesPerResident, setInitialNumInvitesPerResident] =
@@ -41,11 +72,11 @@ const initialize = () => {
 
     const [initialNumParkingSpots, setInitialNumParkingSpots] = useState(0);
 
+    const [numParkingSpotsAvailableToday, setNumParkingSpotsAvailableToday] =
+        useState(0);
+
     // State to track whether the restrictions have changed
     const [restrictionsChanged, setRestrictionsChanged] = useState(false);
-
-    // State for invites for today
-    const [todayInvites, setTodayInvites] = useState(0);
 
 
     const [numParkingSpotsAvailable, setNumParkingSpotsAvailable] = useState(0);
@@ -79,13 +110,7 @@ const initialize = () => {
         }
     `);
 
-    const maxSleepoversQuery = useQuery(gql`
-        query {
-            getMaxSleepovers {
-                value
-            }
-        }
-    `);
+
 
     // Number of invites per resident state
     const [numInvitesPerResident, setNumInvitesPerResident] = useState(1);
@@ -104,41 +129,6 @@ const initialize = () => {
     const numParkingSpotsAvailableQuery = useQuery(gql`
          query {
             getTotalAvailableParking
-        }
-    `);
-
-
-
-    const numInviteInDateRangeQuery = useQuery(
-        gql`
-        query {
-            getNumInvitesPerDate(
-                dateStart: "${start}",
-                dateEnd: "${endDate}"
-            ) {
-                inviteDate
-                inviteState
-            }
-        }
-    `,
-        { fetchPolicy: "no-cache" }
-    );
-
-    const numParkingInDateRangeQuery = useQuery(gql`
-        query {
-            getUsedParkingsInRange(startDate: "${parkingStartDate}", endDate: "${parkingEndDate}") {
-                reservationDate
-            }
-        }
-    `);
-
-    const predictedInvitesQuery = useQuery(gql`
-        query {
-          getPredictedInviteData(startDate: "${startDate}", endDate: "${endDate}") {
-            date
-            parking,
-            visitors
-          }
         }
     `);
 
@@ -260,69 +250,6 @@ const initialize = () => {
             }
         }
 
-        // Num invites in range
-        if (
-            !numInviteInDateRangeQuery.loading &&
-            !numInviteInDateRangeQuery.error
-        ) {
-            const invites = numInviteInDateRangeQuery.data.getNumInvitesPerDate;
-            let numCancelled = 0;
-            invites.forEach((invite) => {
-                if (invite.inviteState === "cancelled") {
-                    numCancelled++;
-                } else if (!isNaN(inviteDateMap.get(invite.inviteDate))) {
-                    inviteDateMap.set(
-                        invite.inviteDate,
-                        inviteDateMap.get(invite.inviteDate) + 1
-                    );
-                }
-            });
-
-            setNumCancel(numCancelled);
-            setAvgVisitors(invites.length / 7);
-
-            setDateMap(new Map(inviteDateMap));
-            setVisitorVals({
-                data: Array.from(inviteDateMap.values()),
-                labels: Array.from(inviteDateMap.keys()),
-                label: "Invites",
-            });
-
-            setTodayInvites(inviteDateMap.get(startDate));
-        } else if (numInviteInDateRangeQuery.error) {
-            console.error(numInviteInDateRangeQuery.error);
-        }
-
-
-        // Num parking in range
-        if (
-            !numParkingInDateRangeQuery.loading &&
-            !numParkingInDateRangeQuery.error
-        ) {
-            const parkingNumbers =
-                numParkingInDateRangeQuery.data.getUsedParkingsInRange;
-
-            parkingNumbers.forEach((parking) => {
-                if (!isNaN(parkingDateMap.get(parking.reservationDate))) {
-                    parkingDateMap.set(
-                        parking.reservationDate,
-                        parkingDateMap.get(parking.reservationDate) + 1
-                    );
-                }
-            });
-
-            setAvgParking(parkingNumbers.length / 7);
-
-            setParkingDateMap(new Map(parkingDateMap));
-            setParkingVals({
-                labels: Array.from(parkingDateMap.keys()),
-                data: Array.from(parkingDateMap.values()),
-                label: "Parking",
-            });
-        } else if (numParkingInDateRangeQuery.error) {
-            console.error(numParkingInDateRangeQuery.error);
-        }
-
         // Parking spots available
         if (
             !numParkingSpotsAvailableQuery.loading &&
@@ -364,8 +291,6 @@ const initialize = () => {
 
     }, [
         numInvitesQuery,
-        numInviteInDateRangeQuery,
-        numParkingInDateRangeQuery,
         numParkingSpotsAvailableQuery,
         setParkingVals,
         setNumParkingSpotsAvailable,
@@ -373,22 +298,6 @@ const initialize = () => {
         numSleepoversPerResidentQuery,
 
     ]);
-
-    useEffect(() => {
-        if (!predictedInvitesQuery.loading && !predictedInvitesQuery.error) {
-            const predictedVisitors = [];
-            const predictedParking = [];
-            predictedInvitesQuery.data.getPredictedInviteData.forEach(
-                (invite) => {
-                    predictedVisitors.push(invite.visitors);
-                    predictedParking.push(invite.parking);
-                }
-            );
-
-            setPredictedVisitorVals(predictedVisitors);
-            setPredictedParkingVals(predictedParking);
-        }
-    }, [predictedInvitesQuery]);
 
     function populateCurfew(){
         if (!CurfewTimeQuery.loading && !CurfewTimeQuery.error) {
@@ -417,18 +326,43 @@ const initialize = () => {
         populateCurfew();
     }, [CurfewTimeQuery]);
 
-
-
-
-
-
-
-
-
     return (
         <Layout>
-            <div className="mb-3 space-y-3 px-3">
+            <div className="mb-3 space-y-3 px-3 pt-9">
                 <div className="grid grid-cols-1 grid-rows-1 space-y-3">
+                    <h1 className="flex flex-col items-center justify-center space-x-3 text-2xl font-bold lg:flex-row">
+                        <span className="mr-3 text-xl text-primary md:text-3xl">
+                            <MdBlock />
+                        </span>{" "}
+                        System Restrictions
+                        <div className="flex items-center">
+                            {restrictionsChanged && (
+                                <div className="flex space-x-1">
+                                    <button
+                                        onClick={saveRestrictions}
+                                        className="btn btn-primary btn-sm space-x-3 lg:btn-md"
+                                    >
+                                        <span>
+                                            <MdDataSaverOn className="mr-3 text-xl" />
+                                        </span>{" "}
+                                        Save Changes
+                                    </button>
+                                    <button
+                                        onClick={cancelRestrictions}
+                                        className="btn btn-secondary btn-sm space-x-3 lg:btn-md"
+                                    >
+                                        <span>
+                                            <MdDataSaverOff className="mr-3 text-xl" />
+                                        </span>{" "}
+                                        Cancel Changes
+                                    </button>
+                                </div>
+                            )}
+                        </div>
+                    </h1>
+
+
+
                     <div className="grid grid-cols-1 gap-4 md:grid-cols-4">
                         <div className="card bg-base-200">
                             <div className="card-body">
@@ -544,11 +478,11 @@ const initialize = () => {
                                             setHours(e.target.value);
                                             setRestrictionsChanged(true);
                                         }}>
-                                            {hours.map((value, index) => (
+                                            {hours.map( (value,index) => (
                                                 value == defaultHours ? (
-                                                    <option selected value={value}>{value}</option>
-                                                ) : (
-                                                    <option value={value}>{value}</option>
+                                                    <option selected value={value}>{value}</option> 
+                                                ):(
+                                                    <option value={value}>{value}</option> 
                                                 )
                                             ))}
                                         </select>
@@ -557,11 +491,11 @@ const initialize = () => {
                                             setMinutes(e.target.value);
                                             setRestrictionsChanged(true);
                                         }}>
-                                            {mins.map((value, index) => (
+                                            {mins.map( (value,index) => (
                                                 value == defaultMins ? (
-                                                    <option selected value={value}>{value}</option>
-                                                ) : (
-                                                    <option value={value}>{value}</option>
+                                                    <option selected value={value}>{value}</option> 
+                                                ):(
+                                                    <option value={value}>{value}</option> 
                                                 )
                                             ))}
                                         </select>
@@ -569,12 +503,70 @@ const initialize = () => {
                                 </div>
                             </div>
                         </div>
+
+                        <div className="card bg-base-200">
+                            <div className="card-body">
+                                <h2 className="card-title">
+                                    Sleepovers{" "}
+                                    <div className="badge badge-secondary">
+                                        Resident
+                                    </div>
+                                </h2>
+                                <p>
+                                    Number of sleepovers a resident is allowed per month
+                                </p>
+                                <div className="card-actions flex items-center justify-start">
+                                    <div className="flex items-center space-x-3">
+                                        <button
+                                            data-testid="increaseSleepovers"
+                                            className="btn btn-circle"
+                                            onClick={() => {
+                                                setMaxSleepovers(
+                                                    maxSleepovers +
+                                                    1
+                                                );
+                                                setRestrictionsChanged(true);
+                                            }}
+                                        >
+                                            <AiOutlinePlus className="text-xl md:text-2xl lg:text-3xl" />
+                                        </button>
+                                        <p
+                                            id="numInvitesPerResident"
+                                            className="text-4xl font-bold text-secondary"
+                                        >
+                                            {maxSleepovers}
+                                        </p>
+                                        <button
+                                            data-testid="decreaseInvites"
+                                            className="btn btn-circle"
+                                            onClick={() => {
+                                                maxSleepovers > 1 &&
+                                                    setMaxSleepovers(
+                                                        maxSleepovers -
+                                                        1
+                                                    );
+                                                setRestrictionsChanged(true);
+                                            }}
+                                        >
+                                            <AiOutlineMinus className="text-xl md:text-2xl lg:text-3xl" />
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="pl-4">Here we set the ammount of invites a resident is allowed to have open at any given time</div>
+                        <div className="pl-4">Here we set the ammount of parking spots our building has availiable</div>
+                        <div className="pl-4">Here we set the time by which visitors must leave the building before they are clasified as sleeping over</div>
+                        <div className="pl-4">Here we set the ammount of sleepovers a resident is allowed per month</div>
+
                     </div>
                 </div>
             </div>
+
         </Layout>
     );
-}
+};
 
 // export async function getStaticProps(context) {
 //     return {
